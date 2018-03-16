fb688849341149eff287caf454bef5c3892139b8
tk-codes <kk.devpro11@gmail.com>
2018-03-16 12:35:36 +0100

    fix console log

--- 1-hello-world-nodejs/app.js
+++ 1-hello-world-nodejs/app.js
@@ -5,4 +5,4 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
